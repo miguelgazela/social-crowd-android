ccc9c4f8696c23d1b433e99d5150902c1d40ec20
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2013-05-27 23:19:43 +0100

    new event done. Falta talvez melhorar o design daquilo. E falta por a opcao de localizacao...

--- Android-App/src/pt/up/fe/socialcrowd/activities/NewEventActivity.java
+++ Android-App/src/pt/up/fe/socialcrowd/activities/NewEventActivity.java
@@ -1,13 +1,29 @@
 package pt.up.fe.socialcrowd.activities;
 
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.json.JSONException;
+
 import pt.up.fe.socialcrowd.R;
 import pt.up.fe.socialcrowd.API.Request;
+import pt.up.fe.socialcrowd.API.RequestException;
+import pt.up.fe.socialcrowd.logic.BaseEvent;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class NewEventActivity extends DashboardActivity {
 
@@ -25,7 +41,7 @@ public class NewEventActivity extends DashboardActivity {
 			@Override
 			public void onClick(View v) {
 				addEvent();
-				
+				startActivity(new Intent(getApplicationContext(), HomeActivity.class));
 			}
 		});
 		
@@ -37,12 +53,81 @@ public class NewEventActivity extends DashboardActivity {
 		new AsyncTask<Void, Void, Void>() {
 			@Override
 			protected Void doInBackground(Void... params) {
+	
+				String name = ((EditText) findViewById(R.id.event_name)).getText().toString();
+				String description = ((EditText) findViewById(R.id.event_description)).getText().toString();
+				String category = ((EditText) findViewById(R.id.event_category)).getText().toString();
+				String singleLineTags = ((EditText) findViewById(R.id.tags_field)).getText().toString();
+				String alltags[] = singleLineTags.split(",");
+				ArrayList<String> tags = new ArrayList<String>();
+				for(String s : alltags){
+					tags.add(s);
+				}
+				
+				RadioGroup rbg = (RadioGroup) findViewById(R.id.newEvent_radioButtonGroup);	
+				RadioButton selected = (RadioButton) findViewById(rbg.getCheckedRadioButtonId());
+				String type = selected.getText().toString();
+				int eventType = 0;
+				
+				if(type == "Private"){
+					eventType = BaseEvent.PRIVATE;
+				}else{
+					if(type == "Public"){
+						eventType = BaseEvent.PUBLIC;
+					}else{
+						if(type == "Geolocated"){
+							eventType = BaseEvent.GEOLOCATED;
+						}
+					}
+				}
+				
+				DatePicker iniDatePicker = (DatePicker) findViewById(R.id.begin_datepicker);
+				DatePicker endDatePicker = (DatePicker) findViewById(R.id.end_datepicker);
+				GregorianCalendar gc = new GregorianCalendar(
+						iniDatePicker.getYear(),
+						iniDatePicker.getMonth(),
+						iniDatePicker.getDayOfMonth());		
+				Date ini = gc.getTime();
+				gc = new GregorianCalendar(
+						endDatePicker.getYear(),
+						endDatePicker.getMonth(),
+						endDatePicker.getDayOfMonth());
+				Date end = gc.getTime();
+				
+				
+				Log.i("eventType", type);
+				Log.i("tags", singleLineTags);
+				Log.i("name", name);
+				Log.i("category", category);
+				Log.i("descrition", description);
+				Log.i("IniDate", ini.toString());
+				Log.i("endDate", end.toString());
+				
+				
 				try {
-					Request.createSession("1");
-				} catch (Exception e) {
+					Request.createEvent(
+							"dummy_id", eventType, name, description,
+							ini, end, null, tags, category);
+				} catch (InvalidParameterException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (RequestException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (JSONException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-//				Request.createEvent(session_id, type, name, description, start_date, end_date, location, tags, category)
+
 				return null;
 			}
 		}.execute();
