61cc6e5b6635a01bcd0276942d391e5cb34b36f1
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2013-05-28 01:52:07 +0100

    search activity quase completa

--- Android-App/src/pt/up/fe/socialcrowd/activities/SearchActivity.java
+++ Android-App/src/pt/up/fe/socialcrowd/activities/SearchActivity.java
@@ -2,7 +2,11 @@ package pt.up.fe.socialcrowd.activities;
 
 import pt.up.fe.socialcrowd.R;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class SearchActivity extends DashboardActivity {
@@ -14,7 +18,7 @@ public class SearchActivity extends DashboardActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_search);
 		setTitleFromActivityLabel(R.id.title_text);
-		/*
+		
 		spinner = (Spinner) findViewById(R.id.search_option_menu);
 		
 		ArrayAdapter<CharSequence> adapter = 
@@ -24,6 +28,24 @@ public class SearchActivity extends DashboardActivity {
 										android.R.layout.simple_spinner_item);
 		
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		spinner.setAdapter(adapter);*/
+		spinner.setAdapter(adapter);
+		
+		Button submit = (Button) findViewById(R.id.search_submit_button);
+		submit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				
+				String parameters = ((EditText) findViewById(R.id.search_input_field)).getText().toString();
+				search(parameters);
+			//startActivity(new Intent(getApplicationContext(), HomeActivity.class));
+			}
+		});
+	}
+		
+		
+
+	private void search(String parameters) {
+		// TODO Auto-generated method stub	
 	}
 }
