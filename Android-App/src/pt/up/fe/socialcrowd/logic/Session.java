1b83ac2f550b516760bbbec110abe466ec5ad643
Miguel Oliveira <miguel.gazela@gmail.com>
2013-05-29 02:36:23 +0100

    Conflitos

--- Android-App/src/pt/up/fe/socialcrowd/logic/Session.java
+++ Android-App/src/pt/up/fe/socialcrowd/logic/Session.java
@@ -2,9 +2,11 @@ package pt.up.fe.socialcrowd.logic;
 
 public class Session {
 	private String session_id;
+	private int user_id;
 	
-	public Session(String sid) {
+	public Session(String sid,int id) {
 	      setSession_id(sid);
+	      setUser_id(id);
 	}
 
 	/**
@@ -19,5 +21,19 @@ public class Session {
 	 */
 	public void setSession_id(String session_id) {
 		this.session_id = session_id;
+	}
+
+	/**
+	 * @return the user_id
+	 */
+	public int getUser_id() {
+		return user_id;
+	}
+
+	/**
+	 * @param user_id the user_id to set
+	 */
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
 	}
 }
